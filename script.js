dc7449a9c9bc96d4b56daa753b7e3ed9ad548574
Glakxic <ogglakxic@gmail.com>
2026-07-24 11:14:13 +0200

    JS fix test

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const btn1 = document.querySelector(".item1");
-btn1.addEventListener("click", function () {
-  fetch("./test.html" /*, options */)
-    .then((response) => response.text())
-    .then((html) => {
-      document.querySelector("html").innerHTML = html;
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-});
+btn1.addEventListener(
+  "click",
+  () => (document.body.innerHTML = generatePage("test", "Pas Content")),
+);
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// 1. Fonction pour générer une page dynamiquement
+function generatePage(title, content) {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>${title}</title>
+        <style>
+          body { font-family: Arial; margin: 20px; }
+          h1 { color: #333; }
+          div { color : #333}
+        </style>
+      </head>
+      <body>
+        <h1>${title}</h1>
+        <div>${content}</div>
+      </body>
+    </html>
+  `;
 }
