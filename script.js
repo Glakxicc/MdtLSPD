02348bc9d17126ca0bb76677e807b76cba282a1c
Glakxic <ogglakxic@gmail.com>
2026-07-24 10:59:59 +0200

    JS fix test

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const btn1 = document.querySelector(".item1");
-btn1.addEventListener("click", () => (btn1.style.display = "none"));
+btn1.addEventListener("click", function () {
+  fetch("./test.html" /*, options */)
+    .then((response) => response.text())
+    .then((html) => {
+      document.querySelector("html").innerHTML = html;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
